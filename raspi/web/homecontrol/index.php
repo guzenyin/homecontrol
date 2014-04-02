e2099fdabb1d5335fa497fbaa0eb1b93c8656022
raspi <Tony@tingting.(none)>
2014-04-03 07:37:27 +0800

    remove shengda

--- raspi/web/homecontrol/index.php
+++ raspi/web/homecontrol/index.php
@@ -5,26 +5,6 @@
 <head>
 <meta http-equiv="Content-type" content="text/html; charset=utf-8" /> 
 <title>Home Control</title>
-<?php
-    require_once dirname(__FILE__).'/GrandCloudStorage.php';
-
-    $access_key = '2W7F1RBYD9JO5KMB0E5PS7YLX';
-    $access_secret = 'NmUwODc5ZjYtNmRiMC00M2FjLWI4OTUtMGU5Y2Y3OGI3N2Vm';
-    $host = 'http://storage.grandcloud.cn';
-
-    $client = new GrandCloudStorage($host);
-    $client->set_key_secret($access_key, $access_secret);
- 
-    $bucket_name = "homecontrol";
-
-    $client->set_bucket($bucket_name);
-    try {
-           $client->put_object("index.php", "index.php"); 
-	} catch(Exception $e) { 
-	   $error = $e->getMessage();
-           var_dump($error);
-	}
-?>
 <script src="./js/bootstrap.min.js" type="text/javascript"></script>
 <script src="./js/jquery-1.8.3.min.js" type="text/javascript"></script>
 </head>
