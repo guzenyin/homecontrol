31426941442a63a527fa9258571a0e9c07c2fbd0
tony <guzenyin@gmail.com>
2014-04-03 22:29:38 +0800

    add controller

--- raspi/web/homecontrol/js/hc.js
+++ raspi/web/homecontrol/js/hc.js
@@ -7,23 +7,3 @@
  */
 
 //const for Pushover;
-var kPushoverUrl = "https://api.pushover.net/1/messages.json";
-
-function sendPushoverMessage(title,message){
-    var postData = JSON.stringify({
-        token:"ae8stbH21emfZD8uBUt5sK6NgLUJ8z",
-        user:"u9mqqPxNdjYoB9NkJdmiSf8djtw8nM",
-        message:message,
-        title:title
-    });
-    $.ajax({
-        url: kPushoverUrl,
-        type:'POST',
-        data:postData,
-        dataType:'jsonp',
-        jsonp:'jsoncallback',
-        success:function(result,status,xhr){
-
-        }
-    });
-}
