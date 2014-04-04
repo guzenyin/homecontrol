02da63a3a3e4bf080f4ae6b32239e26de54c3a73
tony <guzenyin@gmail.com>
2014-04-04 22:44:51 +0800

    schema

--- raspi/web/homecontrol/controller.php
+++ raspi/web/homecontrol/controller.php
@@ -1,28 +1,96 @@
 <?php
-$method = $_SERVER['REQUEST_METHOD'];
-$type = NULL;
 
-if($method === "GET")
+define("kChannelWeb","Web");
+define("kChannelMobile","Mobile");
+define("kChannelOther","Other");
+
+define("kActionTakePicture","TakePicture");
+define("kActionSendMessage","SendMessage");
+
+header('Content-type: text/json');
+$json = null;
+$channel = null;
+$action = null;
+$id = null;
+$data = null;
+$reqdate = null;
+
+$method = $_SERVER['REQUEST_METHOD'];
+if($method === "POST")
 {
-    $type = $_GET["type"];
-    if(!is_null($type)){
-        if(strtolower($type) === "sendmessage"){
-            sendPushoverMessage("test","hello,world");
-        }
+    $json_string = $GLOBALS['HTTP_RAW_POST_DATA'];
+    if(ini_get("magic_quotes_gpc") == "1"){
+        $json_string = stripslashes($json_string);
     }
+    $json = json_decode($json_string);
+}else{
+    echo null;
 }
 
-function sendPushoverMessage($title,$message){
+if(!is_null($json)){
+    $channel = strtolower($json->channel);
+    $action = strtolower($json->action);
+    $data = $json->data;
+    $id = $json->id;
+
+    if(is_null($channel) || is_null($action)){
+        echo null;
+    }elseif($channel === kChannelWeb || $channel === kChannelMobile){
+        processRequest($channel,$action,$data,$id);
+    }else{
+        echo null;
+    }
+}else{
+    echo null;
+}
+
+function processRequest($channel,$action,$data,$id){
+    $errCode = -1;
+    $result = false;
+
+    if($action === kActionTakeCamera){
+        $resultData = takePicture(0);
+        $result = $resultData["result"];
+        $errCode = $resultData["code"];
+    }elseif($action === kActionSendMessage){
+        $resultData = sendMessage("Test","Hello,world!");
+        $result = $resultData["result"];
+        $errCode = $resultData["code"];
+    }
+    $resp = generateResponse($channel,$action,$id,$result,$errCode);
+    echo json_encode($resp);
+}
+
+
+function generateResponse($channel,$action,$id,$result,$errCode){
+    $resp = array (
+        "channel" => $channel,
+        "action" => $action,
+        "id"   => $id,
+        "status" => $result,
+        "errCode" => $errCode
+    );
+    return $resp;
+}
+
+function takePicture($cameraId){
+    return array("result"=>true,"code"=>0);
+}
+
+function sendMessage($title,$message){
+    //now suppport Pushover
     curl_setopt_array($ch = curl_init(), array(
-        CURLOPT_URL => "https://api.pushover.net/1/messages.json",
-            CURLOPT_POSTFIELDS => array(
-                "token" => "ae8stbH21emfZD8uBUt5sK6NgLUJ8z",
-                "user" => "u9mqqPxNdjYoB9NkJdmiSf8djtw8nM",
-                "message" => $message,
-                "title" =>  $title
-            )
+    CURLOPT_URL => "https://api.pushover.net/1/messages.json",
+    CURLOPT_POSTFIELDS => array(
+    "token" => "ae8stbH21emfZD8uBUt5sK6NgLUJ8z",
+    "user" => "u9mqqPxNdjYoB9NkJdmiSf8djtw8nM",
+    "message" => $message,
+    "title" =>  $title
+    )
         ));
     curl_exec($ch);
     curl_close($ch);
+
+    return array("result"=>true,"code"=>0);
 }
 ?>
--- raspi/web/homecontrol/js/hc.js
+++ raspi/web/homecontrol/js/hc.js
@@ -7,3 +7,116 @@
  */
 
 //const for Pushover;
+
+var kDispatchUrl = "controller.php";
+var kChannelWeb = "web";
+var kActionTakePicture = "TakePicture" ;
+var kActionSendMessage = "SendMessage" ;
+
+function start(){
+    $("#btnSendMessage").bind("click",function(e){
+        e.preventDefault();
+        _sendMessage();
+    });
+    $("#btnTakePicture").bind("click",function(e){
+        e.preventDefault();
+        _takePicture();
+    });
+}
+
+function _sendMessage(){
+    doRequest(kActionSendMessage,kDispatchUrl,function(status){},[],null,null,null);
+}
+
+function _takePicture(){
+    doRequest(kActionTakePicture,kDispatchUrl,function(status){},[],null,null,null);
+}
+
+
+///Tool
+Date.prototype.format = function(format){
+    var o = {
+        "M+" : this.getMonth() + 1,
+        "d+" : this.getDate(),
+        "h+" : this.getHours(),
+        "m+" : this.getMinutes(),
+        "s+" : this.getSeconds(),
+        "q+" : Math.floor((this.getMonth() + 3) / 3),
+        "S" : this.getMilliseconds()
+    }
+
+    if (/(y+)/.test(format))
+    {
+        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4
+            - RegExp.$1.length));
+    }
+
+    for (var k in o)
+    {
+        if (new RegExp("(" + k + ")").test(format))
+        {
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1
+                ? o[k]
+                : ("00" + o[k]).substr(("" + o[k]).length));
+        }
+    }
+    return format;
+}
+
+function isFunction(fn) {
+    return !!fn && !fn.nodeName && fn.constructor != String &&
+        fn.constructor != RegExp && fn.constructor != Array &&
+        /function/i.test( fn + "" );
+}
+
+function doAjaxRequestAction(url,data,fnBeforeSend,fnSuccess,fnError,fnComplete) {
+    $.ajax({
+        type:"post",
+        url:url,
+        data:data,
+        dataType:"json",
+        beforeSend:function(xhr,setting){
+            if(isFunction(fnBeforeSend)){
+                fnBeforeSend();
+            }else{
+                $.blockUI();
+            }
+        },
+        success:function(result,status,xhr){
+            if(isFunction(fnSuccess)){
+                fnSuccess(result);
+            }
+        },
+        error:function(xhr,status,errorThrown){
+            if(isFunction(fnError)){
+                fnError();
+            }
+        },
+        complete:function(xhr,status){
+            if(isFunction(fnComplete)){
+                fnComplete();
+            }else{
+                $.unblockUI();
+            }
+        }
+    });
+}
+
+function doGeneratePackage(action,params){
+    var time = new Date();
+    var package = {
+        channel:kChannelWeb,
+        reqId:Math.uuid(),
+        reqDate:time.format("yyyy-MM-dd hh:mm:ss"),
+        action:action,
+        data:params
+    };
+    return JSON.stringify(package);
+}
+
+function doRequest(action,url,fnSuccess,params,fnBeforeSend,fnError,fnComplete){
+    var data = doGeneratePackage(action,params);
+    doAjaxRequestAction(url,data,fnBeforeSend,function(data){
+        fnSuccess(data.status);
+    },fnError,fnComplete);
+}
