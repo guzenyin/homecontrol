af0d2500c3bbfee9a66eff698f3fedc653b91fae
tony <guzenyin@gmail.com>
2014-04-08 22:21:40 +0800

    pushover

--- raspi/web/js/hc.js
+++ raspi/web/js/hc.js
@@ -8,7 +8,7 @@
 
 //const for Pushover;
 
-var kDispatchUrl = "/getway/controller.php";
+var kDispatchUrl = "/getway/action.php";
 var kChannelWeb = "web";
 var kActionTakePicture = "TakePicture" ;
 var kActionSendMessage = "SendMessage" ;
