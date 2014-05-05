c91b101e44e0c60e2e6058cd8adf782995a4ede2
raspi <pi@tingting.(none)>
2014-05-05 22:59:02 +0800

    修改错误的映射关系

--- raspi/sh/systemStart.sh
+++ raspi/sh/systemStart.sh
@@ -7,3 +7,9 @@ cd /home/pi/bin/phddns/
 #start mjpg-streamer
 cd /home/pi/bin/mjpg-streamer/
 sh start.sh
+
+#start flask
+#. /home/pi/venv/bin/activate
+
+#cd /home/pi/homecontrol/raspi/py
+#nohup python systemStart.py >/dev/null 2>&1 &
